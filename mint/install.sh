8cf922949fe7ea574237350ceba8777d3ba68490
TheLonelyCoder <t.krojer@gmail.com>
2026-08-19 15:39:46 +0200

    Update PHP installation to include php-xml package

--- mint/install.sh
+++ mint/install.sh
@@ -13,7 +13,7 @@ sudo apt install -y gh
 
 # Languages and toolchains
 sudo apt install -y fpc 
-sudo apt install -y php 
+sudo apt install -y php php-xml
 sudo apt install -y default-jdk openjdk-8-jdk openjdk-11-jdk
 sudo apt install -y nasm fasm
 
