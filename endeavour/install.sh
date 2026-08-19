f418fa78734b9bca04afde5bf65d3155a5fc1beb
TheLonelyCoder <t.krojer@gmail.com>
2026-08-19 18:50:40 +0200

    Update setup script completion message

--- endeavour/install.sh
+++ endeavour/install.sh
@@ -36,6 +36,6 @@ fi
 codium --install-extension redhat.java
 codium --install-extension bmewburn.vscode-intelephense-client
 
-echo "------------------------------------------"
-echo "Setup script finished."
-echo "------------------------------------------"
+echo "------------------------------------------------------------"
+echo "--- Setup script finished, do your 'Kobayashi Maru' Test ---"
+echo "------------------------------------------------------------"
