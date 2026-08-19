96ef7f4921ddbc818ce6b7ea8b3b69888d7581b9
TheLonelyCoder <t.krojer@gmail.com>
2026-08-19 10:45:06 +0200

    Update install.sh to include more packages
    Added additional packages for development tools and languages.

--- endeavour/install.sh
+++ endeavour/install.sh
@@ -3,20 +3,27 @@
 # Break on error
 set -e
 
-# Update 
+# Update
 sudo pacman -Syu --noconfirm
 
 # Basic tools
-sudo pacman -S --needed --noconfirm git wget curl dos2unix
+sudo pacman -S --needed --noconfirm git wget curl gnupg dos2unix
 sudo pacman -S --needed --noconfirm github-cli
 
 # Languages and toolchains
-sudo pacman -S --needed --noconfirm jdk-openjdk php 
+sudo pacman -S --needed --noconfirm fpc
+sudo pacman -S --needed --noconfirm php
 sudo pacman -S --needed --noconfirm fasm nasm
-sudo pacman -S --needed --noconfirm fpc 
+sudo pacman -S --needed --noconfirm jdk-openjdk
+sudo pacman -S --needed --noconfirm jdk8-openjdk
+sudo pacman -S --needed --noconfirm jdk11-openjdk
 
 # Development tools
-sudo pacman -S --needed meld
+sudo pacman -S --needed --noconfirm lazarus
+sudo pacman -S --needed --noconfirm maven
+
+# Development tools
+sudo pacman -S --needed --noconfirm meld
 sudo pacman -S --needed --noconfirm lazarus
 
 if ! command -v codium >/dev/null 2>&1
