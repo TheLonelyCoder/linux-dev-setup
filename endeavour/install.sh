5f804513591c65779dc4f6737797ccd0cab32b9f
TheLonelyCoder <t.krojer@gmail.com>
2026-08-19 08:16:58 +0200

    Add GitHub CLI installation to install.sh

--- endeavour/install.sh
+++ endeavour/install.sh
@@ -8,6 +8,7 @@ sudo pacman -Syu --noconfirm
 
 # Basic tools
 sudo pacman -S --needed --noconfirm git wget curl dos2unix
+sudo pacman -S --needed --noconfirm github-cli
 
 # Languages and toolchains
 sudo pacman -S --needed --noconfirm jdk-openjdk php 
