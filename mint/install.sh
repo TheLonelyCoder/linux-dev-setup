1fae6fa8827bca61acd94afa55f4410fc3b6db7c
TheLonelyCoder <t.krojer@gmail.com>
2026-08-19 15:07:21 +0200

    Install nano and vim in install.sh
    Added nano and vim to the list of installed tools.

--- mint/install.sh
+++ mint/install.sh
@@ -7,7 +7,8 @@ set -e
 sudo apt update
 
 # Basic tools
-sudo apt install -y git wget curl gpg dos2unix meld
+sudo apt install -y git wget curl gpg dos2unix meld 
+sudo apt install -y nano vim
 sudo apt install -y gh
 
 # Languages and toolchains
