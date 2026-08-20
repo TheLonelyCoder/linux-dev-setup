3c34c2c70e65ca19a8b0e39a12a6d6dd9f855e0b
TheLonelyCoder <t.krojer@gmail.com>
2026-08-20 08:08:13 +0200

    Add installation for c3c and ldc packages

--- mint/install.sh
+++ mint/install.sh
@@ -19,6 +19,8 @@ sudo apt install -y nasm fasm
 sudo apt install -y dotnet-sdk-10.0 
 sudo apt install -y libgtk-3-dev libgtk-4-dev 
 sudo apt install -y sqlite3 libsqlite3-dev
+sudo apt install -y c3c
+sudo apt install -y ldc
 
 # Development tools
 sudo apt install -y lazarus
