52e0a8f1c2ed185f2d4519f99e34a85952cab0f9
TheLonelyCoder <t.krojer@gmail.com>
2026-09-16 17:25:41 +0200

    Add installation of valac-bin package

--- mint/install.sh
+++ mint/install.sh
@@ -25,6 +25,7 @@ sudo apt install -y dotnet-sdk-10.0
 sudo apt install -y libgtk-3-dev libgtk-4-dev 
 sudo apt install -y sqlite3 libsqlite3-dev
 sudo apt install -y ldc
+sudo apt install -y valac-bin
 sudo apt install -y nim
 
 # C3 compiler
