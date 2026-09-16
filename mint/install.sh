9f94993b451c4f726df646137de48391101e5d3a
TheLonelyCoder <t.krojer@gmail.com>
2026-09-16 17:16:33 +0200

    Add installation command for Nim

--- mint/install.sh
+++ mint/install.sh
@@ -25,6 +25,7 @@ sudo apt install -y dotnet-sdk-10.0
 sudo apt install -y libgtk-3-dev libgtk-4-dev 
 sudo apt install -y sqlite3 libsqlite3-dev
 sudo apt install -y ldc
+sudo apt install -y nim
 
 # C3 compiler
 curl -fsSL https://raw.githubusercontent.com/c3lang/c3c/refs/heads/master/install/install.sh | bash
